4e0b9ebb9c4598e77142d53384bb3a9248183dd5
Patrick Ryan <167943501+patrickryan331@users.noreply.github.com>
2024-09-24 14:56:57 -0400

    first commit

---
+++ pages/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+
+def home_view(request):
+    return render(request, 'pages/home.html')
+
+def about_view(request):
+    return render(request, 'pages/about.html')
