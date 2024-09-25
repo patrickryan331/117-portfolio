fe16509a0fe974f1959d351874a3773b5524d5b0
Patrick Ryan <167943501+patrickryan331@users.noreply.github.com>
2024-09-24 22:44:29 -0400

    Assignment -1

--- content/urls.py
+++ content/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from .views import projects_list, experience_list
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('', projects_list, name='projects_list' ),
-    path('experience_list', experience_list, name='experience_list' ),
+    path('experience', experience_list, name='experience_list' ),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- content/views.py
+++ content/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-
+from .models import Project
 
 
 def projects_list(request):
-    return render(request, 'content/projects_list.html')
+
+    projects = Project.objects.all()
+
+    return render(
+                request,
+                'content/projects_list.html',
+                {"projects": projects}
+                )
 
 def experience_list(request):
     return render(request, 'content/experience_list.html')
